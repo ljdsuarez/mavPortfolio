6c535c2ab9aed4780bcddec7c8ae22e9601b73bf
Laurenz <ljdsuarez@gmail.com>
2023-10-12 16:00:24 +0300

    furnish carousel

--- script.js
+++ script.js
@@ -223,6 +223,38 @@ if (navContainer.dataset.navContainer == "mobile") {
 
 //URLs of files in ./public folder
 let content1 = ["1.png", "2.png", "3.png"];
+let content2 = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+let cosmetics = [
+  "1.png",
+  "2.png",
+  "3.png",
+  "4.png",
+  "5.png",
+  "6.png",
+  "7.png",
+  "8.png",
+  "9.png",
+  "10.png",
+  "11.png",
+  "12.png",
+  "13.png",
+  "14.png",
+  "15.png",
+  "16.png",
+  "17.png",
+  "18.png",
+  "19.png",
+  "20.png",
+  "21.png",
+  "22.png",
+  "23.png",
+  "24.png",
+  "25.png",
+  "26.png",
+  "27.png",
+  "28.png",
+];
+let logos = ["1.png", "2.png", "3.png", "4.png"];
 
 const arrowLeft = document.getElementById("arrow-left");
 const arrowRight = document.getElementById("arrow-right");
@@ -294,4 +326,4 @@ function workName(folderName, folderArray) {
   };
 }
 
-workName("content1", content1);
+workName("cosmetics", cosmetics);
